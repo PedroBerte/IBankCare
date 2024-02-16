13d50c6410adef18e1e7755ac9bb29568e1198d9
Pedro Bertelli <pedrobertelli19@gmail.com>
2024-02-15 21:27:05 -0300

    Inicio do projeto e configuração dos temas.

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JS3Y7kEyEwokMP5eiv9nPa6IRBXE2XAmwg8EYFeWmJ4=\"\n}"
---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="bg-background w-full h-screen">
+      <div className="bg-white">
+        <p className="text-secondary">Bem-vindo de Volta!</p>
+      </div>
+    </div>
+  );
+}
